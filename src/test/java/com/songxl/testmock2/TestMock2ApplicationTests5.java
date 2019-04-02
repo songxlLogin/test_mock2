1cb3b1a78c0378500736d26f098bf073a050c79c
songxl <1229348133@qq.com>
2019-04-02 20:43:43 +0800

    test doAnswer

--- src/test/java/com/songxl/testmock2/TestMock2ApplicationTests5.java
+++ src/test/java/com/songxl/testmock2/TestMock2ApplicationTests5.java
@@ -2,17 +2,20 @@ package com.songxl.testmock2;
 
 import com.songxl.testmock2.dao.UserDAO;
 import com.songxl.testmock2.dao.impl.UserDAOImpl;
-import com.songxl.testmock2.service.UserService;
 import com.songxl.testmock2.service.impl.UserServiceImpl;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import static org.mockito.Mockito.when;
@@ -22,7 +25,7 @@ import static org.mockito.Mockito.when;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TestMock2ApplicationTests5 {
+public class TestMock2ApplicationTests5 implements Answer<String> {
 
     @Before
     public void setUp() {
@@ -36,7 +39,21 @@ public class TestMock2ApplicationTests5 {
     private UserDAO userDAO = new UserDAOImpl();
 
     @Test
-    public void testPrivate(){
+    public void testDoAnswer2() {
+        Mockito.doAnswer(new TestMock2ApplicationTests5()).when(userDAO).getUserById("hi");
+        String id = userServiceImpl.queryUserById("hi");
+        Assert.assertEquals("hi:sxl", id);
+    }
+
+    @Override
+    public String answer(InvocationOnMock invocation) throws Throwable {
+        Object[] arguments = invocation.getArguments();
+        String id = (String) arguments[0];
+        return id+":sxl";
+    }
+
+    @Test
+    public void testPrivate() {
         try {
             when(userDAO.getUserById("hi Micheal")).thenReturn("hello");
             Class<? extends UserServiceImpl> aClass = userServiceImpl.getClass();
@@ -44,7 +61,7 @@ public class TestMock2ApplicationTests5 {
             method.setAccessible(true);
 //            UserServiceImpl userService = aClass.newInstance();
             String test = (String) method.invoke(userServiceImpl, "hi Micheal");
-            Assert.assertEquals("测试结果：","hello",test);
+            Assert.assertEquals("测试结果：", "hello", test);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -57,9 +74,18 @@ public class TestMock2ApplicationTests5 {
         Assert.assertEquals("hi", id);
     }
 
-
-
-
+    @Test
+    public void testDoAnswer() {
+        when(userDAO.getUserById(Mockito.anyString())).thenAnswer((Answer<String>) invocation -> {
+            //所有的入参
+            Object[] arguments = invocation.getArguments();
+            String id = (String) arguments[0];
+            System.out.println("receive:"+id);
+            return "hello";
+        });
+        String id = userServiceImpl.queryUserById("hi");
+        Assert.assertEquals("hello", id);
+    }
 
 
 }
